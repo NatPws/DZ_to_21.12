d7390fea16d7e7d248555246b7b759edf1523caa
Natalia <natalia.ab.ca@gmail.com>
2023-12-19 16:14:48 +0300

    task3 не получилось вывести найденные числа в формате double

---
+++ task3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте массив из вещественных чисел с ненулевой дробной частью. Найдите разницу между максимальным и минимальным элементов массива
+
+double[] array = { 3.2, 8.4, 5.1, 1,9, 6.3};
+int size = 5;
+int i = 0;
+double maxNumber = 0;
+double minNumber = 0;
+double diff = 0;
+{
+    for (i = 0; i < size; i++)
+    {
+    if (array[i] > array[i + 1])
+        {
+        maxNumber = array[i];
+        }
+        else
+        {
+        minNumber = array[i];
+        }
+    }
+    
+    diff = maxNumber - minNumber;
+}
+
+Console.WriteLine($"Разница между максимальным числом {maxNumber} и минимальным числом {minNumber} в массиве составляет: {diff}");
